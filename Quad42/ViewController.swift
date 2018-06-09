73e8a177915ae070d71c4ddb0cb92395f428f478
Saurabh Sikka <saurabh.sikka@ericsson.com>
2018-06-09 15:06:48 +0200

    Initial Commit

---
+++ Quad42/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quad42
+//
+//  Created by Sauron on 09/06/18.
+//  Copyright © 2018 Sauron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
